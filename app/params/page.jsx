05bea3583dac636ab6b40d460b90ba85ad787970
Vlad Gerasimov <vlad@vlad.studio>
2024-10-20 13:22:05 +0400

    searchParams

--- app/params/page.jsx
+++ app/params/page.jsx
@@ -1,7 +1,12 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 export default async function Page({ searchParams }) {
+  const searchParamsFromHook = Object.fromEntries(useSearchParams()?.entries());
   return (
     <>
-      <p>searchParams: {JSON.stringify(await searchParams, null, 2)}</p>
+      <h3>searchParams</h3>
+      <p>from server: {JSON.stringify(await searchParams, null, 2)}</p>
+      <p>from client: {JSON.stringify(searchParamsFromHook, null, 2)}</p>
     </>
   );
 }
