d5be7c3076b5955b8e1719d09e7c24259f256d37
Vlad Gerasimov <vlad@vlad.studio>
2024-10-20 13:24:14 +0400

    ..

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,7 @@ export default async function Page({ children }) {
             {!!user?.isLoggedin ? (
               <>
                 <p>Hey, {user.email}!</p>
+                <div className="flex-1"></div>
                 <form action={signOutAction}>
                   <button type="submit">Sign out</button>
                 </form>
