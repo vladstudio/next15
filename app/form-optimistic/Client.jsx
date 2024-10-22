813b60d56b8620a30804c7494c2fdfe2c7ecc7ca
Vlad Gerasimov <vlad@vlad.studio>
2024-10-22 09:09:42 +0400

    figuring out optimistic form

---
+++ app/form-optimistic/Client.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState, useActionState, useEffect } from "react";
+import { addTodoAction } from "./actions";
+import Form from "next/form";
+
+export function Client({ serverTodos }) {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    setTodos(serverTodos);
+  }, [serverTodos]);
+
+  const [newTodoText, setNewTodoText] = useState("");
+  const [result, _action, isPending] = useActionState(addTodoAction, null);
+  const addFutureTodo = (value) => {
+    setTodos((prev) => [...prev, { text: value }]);
+    setNewTodoText("");
+  };
+  return (
+    <>
+      <ul>{!!todos && todos.map((x, id) => <li key={id}>{x.text}</li>)}</ul>
+      <Form
+        action={_action}
+        onSubmit={(e) => {
+          addFutureTodo(newTodoText);
+        }}
+        className="grid gap-2"
+      >
+        <p>Add todo:</p>
+        <input
+          name="text"
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
+        />
+        <p>
+          <input type="checkbox" name="simulateError" value="1" /> Simulate
+          error
+        </p>
+        <button type="submit"> Add </button>
+      </Form>
+      <p>isPending: {JSON.stringify(isPending, null, 2)}</p>
+      <p>result: {JSON.stringify(result, null, 2)}</p>
+    </>
+  );
+}
--- app/form-optimistic/actions.js
+++ app/form-optimistic/actions.js
@@ -3,24 +3,31 @@ import { cookies } from "next/headers";
 import { setTimeout } from "timers/promises";
 import { revalidatePath } from "next/cache";
 
-export async function fetchTodos() {
+async function getTodosFromCookies() {
   const cookieStore = await cookies();
-  return JSON.parse(cookieStore.get("todos")?.value) || [
-    { text: "do this" },
-    { text: "do that" },
-  ];
+  return JSON.parse(cookieStore.get("todos")?.value || null) || [];
+}
+
+export async function fetchTodosAction() {
+  console.log("fetchTodosAction");
+  await setTimeout(1000);
+  const todos = await getTodosFromCookies();
+  console.log("todos", todos);
+  return todos;
 }
 
 export async function addTodoAction(prevState, rawFormData) {
   const formData = Object.fromEntries(rawFormData);
+  const cookieStore = await cookies();
   try {
     await setTimeout(1000);
     if (!!formData.simulateError) throw new Error("Test");
-    return {
-      text: formData.text,
-    };
-  } catch (e) {
+    const newTodo = { text: formData.text };
+    const newTodos = [...(await getTodosFromCookies()), newTodo];
+    cookieStore.set("todos", JSON.stringify(newTodos));
     revalidatePath("/form-optimistic");
+    return newTodo;
+  } catch (e) {
     return { error: e.message };
   }
 }
